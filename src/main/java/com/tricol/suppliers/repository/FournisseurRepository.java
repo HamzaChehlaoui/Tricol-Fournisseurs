806ea2e5acdfbec76dbae9f3a46731d825f288bb
HamzaChehlaoui <hamzachehlaoui3@gmail.com>
2025-10-24 12:29:55 +0100

    add repository

---
+++ src/main/java/com/tricol/suppliers/repository/FournisseurRepository.java
@@ -0,0 +1,14 @@
+package com.tricol.suppliers.repository;
+
+import com.tricol.suppliers.domain.Fournisseur;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FournisseurRepository extends JpaRepository<Fournisseur, Long> {
+    List<Fournisseur> findBySocieteContainingIgnoreCaseOrderBySocieteAsc(String societe);
+    List<Fournisseur> findByEmailEndingWithIgnoreCase(String domain);
+    boolean existsByIce(String ice);
+}
+
+
